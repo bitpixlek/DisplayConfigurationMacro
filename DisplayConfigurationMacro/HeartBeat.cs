71dde3c05e764b0094b7ecb829879f75bd1fbd79
Marcelo Almeida <Mfelipe7@live.com>
2023-07-27 02:44:34 -0300

    feat : adding service core

---
+++ DisplayConfigurationMacro/HeartBeat.cs
@@ -0,0 +1,37 @@
+﻿using DisplayConfigurationMacro.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DisplayConfigurationMacro
+{
+    public class HeartBeat
+    {
+        private readonly Timer _timer;
+
+        public HeartBeat()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += TimerEvent;
+        }
+
+        private void TimerEvent(object sender, ElapsedEventArgs e)
+        {
+            ServiceMain service = new ServiceMain();
+            service.MainExecution();
+        }
+
+        public void Start()
+        {
+            this._timer.Start();
+        }
+
+        public void Stop()
+        {
+            this._timer.Stop();
+        }
+    }
+}
--- DisplayConfigurationMacro/Program.cs
+++ DisplayConfigurationMacro/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
 
 namespace DisplayConfigurationMacro
 {
@@ -10,8 +11,22 @@ namespace DisplayConfigurationMacro
     {
         static void Main(string[] args)
         {
+            var exitCode = HostFactory.Run(x =>
+            {
+                x.Service<HeartBeat>(s =>
+                {
+                    s.ConstructUsing(heartBeat => new HeartBeat());
+                    s.WhenStarted(heartBeat => heartBeat.Start());
+                    s.WhenStopped(heartBeat => heartBeat.Stop());
+                });
 
+                x.RunAsLocalSystem();
+                x.SetServiceName("DigitalVibranceWatcher");
+                x.SetDisplayName("*Digital Vibrance Watcher*");
+            });
 
+            int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetType());
+            Environment.ExitCode = exitCodeValue;
         }
     }
 }
