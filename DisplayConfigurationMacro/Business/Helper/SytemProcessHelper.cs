07275a030b6c37e9fd7fd633c734e49c62b73991
Marcelo Almeida <Mfelipe7@live.com>
2023-07-26 22:15:05 -0300

    feat: adding system process verification

---
+++ DisplayConfigurationMacro/Business/Helper/SytemProcessHelper.cs
@@ -0,0 +1,21 @@
+﻿using DisplayConfigurationMacro.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayConfigurationMacro.Business.Helper
+{
+    public class SytemProcessHelper
+    {
+        public static bool ProcessOnListIsRunning(ConfigurationParameterModel config)
+        {
+            List<Process> localProcess = Process.GetProcesses().ToList<Process>();
+            List<Process> filteredProcess = localProcess.Select(x => x).Where(x => config.PIDProcess.Contains(x.ProcessName)).ToList();
+            if (filteredProcess.Count > 1) return true;
+            return false;
+        }
+    }
+}
---
+++ DisplayConfigurationMacroTest/Business/Helper/SytemProcessHelperTest.cs
@@ -0,0 +1,22 @@
+﻿using DisplayConfigurationMacro.Business.Helper;
+using DisplayConfigurationMacro.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayConfigurationMacroTest.Business.Helper
+{
+    [TestClass]
+    public class SytemProcessHelperTest
+    {
+        [TestMethod]
+        public void ProcessOnListIsRunning()
+        {
+            ConfigurationParameterModel jsonConfiguration = ConfigurationParametersHelper.GetConfiguration();
+            SytemProcessHelper.ProcessOnListIsRunning(jsonConfiguration);
+        }
+    }
+}
